6fa58fce4ed42babc76584ccf6abceba906d66dc
zhangqi12138 <1078198775@qq.com>
2018-06-26 14:47:21 +0800

    add library

---
+++ app/src/main/java/zqlibrary/mobile/com/cn/zqlib/MainActivity.java
@@ -0,0 +1,14 @@
+package zqlibrary.mobile.com.cn.zqlib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
